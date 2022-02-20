b04c5b0fa98781e846b940544a2325379c0f6331
Vi Vo <aaronvo118@gmail.com>
2022-02-20 08:34:10 -0600

    done

--- src/components/App.js
+++ src/components/App.js
@@ -17,14 +17,22 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+function About(){
+  return (
+    <div id="about">
+      <h2>about</h2>
+    </div>
+
+  );
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
